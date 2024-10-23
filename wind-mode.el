97dc616fe671fad209617940b80328cf05f2071c
wang <wangjiannuaa@outlook.com>
2024-10-23 03:00:56 +0000

    pulse1

--- wind-mode.el
+++ wind-mode.el
@@ -1,4 +1,4 @@
-;;; wind-mode.el --- Support for computer programming language  -*- lexical-binding: t; -*-
+;;; wind.el --- Support for computer programming language  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2024-2025 Jian .P. Wang
 
@@ -17,89 +17,113 @@
 ;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 (require 'semantic)
 (require 'imenu)
+(require 'also-common)
+
+;;;###autoload
+(defun wind-open-windows (arg)
+  "open windows for wind speculators"
+  (interactive "bbuffer:")
+  (make-frame-command)
+ ;;;   (let* (of (selected-frame))
+ ;;;         (nf (make-frame-command))
+;;;          (font1 (frame-parameter of 'font)))
+
+;;;    (save-selected-window (select-frame nf) (set-frame-font font1))
+ ;;;   (message "open wind windows %s." arg)
+    
+)
+
+;;;###autoload
+(defun wind-close-windows (buffer)
+  "close windows for wind speculators"
+  (interactive "bbuffer:")
+;;   (interactive "@\n" (let curframe (selected-frame)))
+;;   (delete-frame curframe)
+)
+
+
+
+
+
+(defgroup wind-tech nil
+  "Wind Technique."
+  :prefix "wind-"
+  :group 'convenience
+  )
+
+
+
+
+
+
+(defvar wind-mode nil
+  "Dummy variable to suppress compiler warnings.")
 
 ;;;###autoload
 (define-minor-mode wind-mode
   "wind mode"
   :init-value  nil
-  :lighter " WD"
-  :keymap '(([C-w-~] . openwindwindows) ([C-w-!] . closewindwindows))
-  :group 'wind-mode-group
+  :lighter " ST"
+  :group 'wind-tech
   (if wind-mode
       (progn
-        (wd-setup)
-        (add-hook 'pre-command-hook 'wd-handle-pre-command nil t)
-        (add-hook 'powd-command-hook 'wd-handle-post-command nil t)
-        (add-hook 'after-save-hook 'wd-clear-variables-after-save nil t)
+        (wind-setup)
+        (add-hook 'pre-command-hook 'wind-handle-pre-command nil t)
+        (add-hook 'post--command-hook 'wind-handle-post-command nil t)
+        (add-hook 'after-save-hook 'wind-clear-variables-after-save nil t)
         (run-hooks 'wind-mode-hook))
-    (remove-hook 'pre-command-hook 'wd-handle-pre-command t)
-    (remove-hook 'post-command-hook 'wd-handle-post-command t)
-    (remove-hook 'after-save-hook 'wd-clear-variables-after-save t)
-    (wd-abort))
+    (remove-hook 'pre-command-hook 'wind-handle-pre-command t)
+    (remove-hook 'post-command-hook 'wind-handle-post-command t)
+    (remove-hook 'after-save-hook 'wind-clear-variables-after-save t)
+    (wind-abort))
   )
 
-(defun wd-handle-pre-command()
+(defun wind-handle-pre-command ()
   (message "pre")
-  )
-(defun wd-handle-post-command()
+)
+(defun wind-handle-post-command ()
   (message "post")
-  )
-  (defun wd-clear-variables-after-save()
-    (message "clear")
-  )
+)
+(defun wind-clear-variables-after-save ()
+  (message "clear")
+)
+(defun wind-mode-hook ()
+  (message "Im in wind-mode")
+)
+(defun wind-setup ()
+  "define keymap"
+  (message "Im in wind-setup")
+  (define-key wind-mode-map (kbd "C-M-*") #'wd-open-windows)
+  (define-key wind-mode-map (kbd "C-M-!") #'wd-close-windows)
 
 
-(defun wind-mode-hook()
-  (message "I'm in wind-mode")
+  (message "Im out wind-abort")
+)
+(defun wind-abort ()
+  "undefine keymap"
+  (message "Im in wind-abort")
 )
 
 
-(defun wd-setup()
-  (message "I'm in wd-setup"))
-(defun wd-abort()
-  (message "I'm in wd-abort"))
-
-
-;;;###autoload
-(defun openwindwindows()
-"open wind windows for wind feeling."
-(message "open wind windows.")
-   (let* (of (selected-frame))
-         (nf (make-frame-command))
-         (font1 (frame-parameter of 'font))
-   )
-
-  ;;;(save-selected-window (select-frame of) (set-frame-font font1))
-)
-
-;;;###autoload
-(defun closewindwindows()
-"close wind windows for wind feeling."
-(message "close wind windows.")
-)
-
-
-(defun apropos (cur buffer)
- "a VR target"
- (message "target:%s" 'cur) 
+(defun wind-apropos (cur buffer)
+ "a MR target"
 )
 (defun wind (context)
-  "basic operator"
-  (message "basic operator to %s" 'context)
-   ;;;(semantic-analyze-contex context)
+  "advanced operator"
 )
 
-
-(defun imenuextforwind (buffer)
-  "interactive vr with cursor"
-  (interactive "P\nb\nenter a buffer name:")
-  (let* (cur (point))
-        (curtext (apropos cur buffer)))
-  (wind curtext) 
+;;;###autoload
+(defun imenu-extension-for-wind (buffer)
+  "interactive ar-aided vr-targeted mr-selection with cursor"
+   (interactive "P\nb\nenter a buffer name:")
+   (let* (cur (point))
+         (curtext (apropos cur buffer))
+	 (wind curtext)
+   )
 )
 
 ;;;(defgroup)
 ;;;(defcustom)
-(global-set-key (kbd "C-M-.") 'imenuextforwind)
+(global-set-key (kbd "C-M-,") 'imenu-extension-for-wind)
 
 (provide 'wind-mode)
